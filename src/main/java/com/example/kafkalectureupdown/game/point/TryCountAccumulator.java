10ec62237e61de1f4676ea3bcd518c7674440f6e
cla9 <cla9@nate.com>
2023-07-14 16:01:13 +0900

    추가점수 전략 추가

--- src/main/java/com/example/kafkalectureupdown/game/point/TryCountAccumulator.java
+++ src/main/java/com/example/kafkalectureupdown/game/point/TryCountAccumulator.java
@@ -2,13 +2,13 @@ package com.example.kafkalectureupdown.game.point;
 
 public class TryCountAccumulator extends AbstractAccumulator{
     public TryCountAccumulator() {
-        points.add(new Point(1, 80));
-        points.add(new Point(3, 70));
-        points.add(new Point(5, 60));
-        points.add(new Point(7, 55));
-        points.add(new Point(8, 50));
-        points.add(new Point(9, 40));
-        points.add(new Point(10, 20));
+        points.add(new Point(1, 100));
+        points.add(new Point(2, 70));
+        points.add(new Point(3, 60));
+        points.add(new Point(4, 55));
+        points.add(new Point(6, 50));
+        points.add(new Point(8, 40));
+        points.add(new Point(10, 30));
         points.add(new Point(11, 15));
         points.add(new Point(12, 6));
         points.add(new Point(15, 3));
